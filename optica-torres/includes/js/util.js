868cb0f56844022791460005b7303abab4cecf57
Darío <prometeo84@gmail.com>
2023-09-18 11:24:02 -0500

    Update util.js
    ERROR

--- optica-torres/includes/js/util.js
+++ optica-torres/includes/js/util.js
@@ -63,7 +63,7 @@ function usernameValidate(evt) {
     return false;
   }
 }
-function emailValidate(input) {
+function emailValidate(input.toString()) {
   var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (input.match(validRegex)) {
     return true;
