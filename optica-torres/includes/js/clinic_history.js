c611fdb12f10703b17e8afe8e20691150908c387
Darío <prometeo84@gmail.com>
2024-04-05 10:51:42 -0500

    Update clinic_history.js

--- optica-torres/includes/js/clinic_history.js
+++ optica-torres/includes/js/clinic_history.js
@@ -22,6 +22,36 @@ function modSearchPatient() {
     return false;
 }
 
+function loadTableSearch(offset_pag, active_pag) {
+    var s_id_number = $('#s_id_number').val();
+    var s_lastname = $('#s_lastname').val();
+    var s_name = $('#s_name').val();
+    var funcion = '../controller/anamnesis_controller.php';
+    var limite_pag = 5;
+    $.ajax({
+        type: 'POST',
+        url: funcion,
+        data: { function: 'loadTableSearchPatients', limit: limite_pag, offset: offset_pag, active_p: active_pag, id_number: s_id_number, name: s_name, lastname: s_lastname },
+        cache: false,
+        beforeSend: function (xhr) {
+            if (s_id_number == '' && s_lastname == '' && s_name == '') {
+                alertPopUp(translate['advertice'], translate['required_fields'], 'warning');
+                xhr.abort();
+            } else {
+                $('#loading').show();
+            }
+        },
+        success: function (data) {
+            $('#loading').hide();
+            $('#tablaSearchPatient').html(data);
+        },
+        error: function (xhr, status) {
+            $('#loading').hide();
+            alertPopUp(translate['error'], translate['error_execution_proccess'], 'error');
+        }
+    });
+}
+
 function showPatientEditFrame(id) {
     $('#modSearchPatient').modal('hide');
     $('#patient_id').val(id);
